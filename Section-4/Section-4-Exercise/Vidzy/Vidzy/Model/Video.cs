66368a4d717d14768ed05e920f57d1bb93b64dad
Alejandro Lezama <alexlezama90@gmail.com>
2019-06-18 15:17:20 -0500

    Uploading Section 4 Solution p2

---
+++ Section-4/Section-4-Exercise/Vidzy/Vidzy/Model/Video.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vidzy.Model
+{
+    public class Video
+    {
+        //public Video()
+        //{
+        //    Genres = new HashSet<Genre>();
+        //}
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime ReleaseDate { get; set; }
+
+        //public ICollection<Genre> Genres { get; set; }
+        public Genre Genre { get; set; }
+        public Classification Classification { get; set; }
+    }
+}
---
+++ Section-4/Section-4-Exercise/Vidzy/Vidzy/VidzyContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vidzy.Model;
+
+namespace Vidzy
+{
+    public class VidzyContext : DbContext
+    {
+        public VidzyContext()
+            :base("name=VidzyContext")
+        {
+        }
+
+        public virtual DbSet<Video> Videos { get; set; }
+        public virtual DbSet<Genre> Genres { get; set; }
+
+    }
+}
